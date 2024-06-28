8f44304066e71e382d8a7e24ffb493051c37d989
alex2000d <demarcoalessandro777@gmail.com>
2024-06-28 16:42:41 +0200

    [UPD] I retrieve the button from the dom

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
 // creo un arrey
 const email = ["giorgio@gmail.com", "alessandro@gmail.com", "andrea@gmail.com", "francesco@gmail.com", "giulia@gmail.com"];
 
+// recupero il pulsante dal dom
+let button = document.getElementById('search');
+
