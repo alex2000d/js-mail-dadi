ab558f620bc2585c7c87eec00414a4d0a1d4f1b4
alex2000d <demarcoalessandro777@gmail.com>
2024-06-28 18:06:39 +0200

    [UPD] exercise complete

---
+++ dadi/js-2/script.js
@@ -0,0 +1,19 @@
+// creo un generatore random con la libreria math
+let player = ((Math.random() * 6 ).toFixed());
+console.log(player)
+
+let computer = ((Math.random() * 6 ).toFixed());
+console.log(computer)
+// se il player ha il numero piu alto allora comparirà la scritta hai vinto in console
+if(player > computer){
+    console.log('hai vinto')
+    
+}
+// se il computer ha il numero piu alto allora comparirà la scritta in console hai perso
+else if(player < computer){
+    console.log('hai perso')
+}
+// altrimenti comparirà la scritta pareggio in console
+else{
+    console.log('pareggio')
+}
